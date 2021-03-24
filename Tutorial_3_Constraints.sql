4c0f883a8ee46a90a2d1351ee498db9a77b20e51
euguroglu <e.uguroglu@yahoo.com>
2021-03-24 18:35:35 +0300

    constraints

---
+++ Tutorial_3_Constraints.sql
@@ -0,0 +1,30 @@
+DROP TABLE student;
+SELECT * FROM student;
+
+# Not null and Unique constraints
+CREATE TABLE student (
+	student_id INT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) UNIQUE,
+    PRIMARY KEY(student_id)
+);
+
+ #Default Constraints
+CREATE TABLE student (
+	student_id INT,
+    name VARCHAR(20) NOT NULL,
+    major VARCHAR(20) DEFAULT 'Undecided',
+    PRIMARY KEY(student_id)
+);
+
+#Auto Primary Key Increment
+CREATE TABLE student (
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),
+    major VARCHAR(20)
+);
+
+INSERT INTO student(name, major) VALUES('Alex','Physic');
+INSERT INTO student(name, major) VALUES('Osman','Engineer');
+
+SELECT * FROM student;
