6c77228711b669f03dbcf8365eb9482095c0c07a
euguroglu <e.uguroglu@yahoo.com>
2021-03-24 18:18:09 +0300

    insert

---
+++ Tutorial_2_Insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO student VALUES(1, 'Alex', 'Physic');
+INSERT INTO student VALUES(2, 'Jennifer', 'Biology');
+INSERT INTO student(student_id, name) VALUES(3, 'Osman');
+SELECT * FROM student;
